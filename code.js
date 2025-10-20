a83c95c23ab8bb7667adb3079ab545534a0347bd
dev-AlexSv <janalexandersilvestree@gmail.com>
2025-10-20 14:21:57 +0800

    Inital Layout and Testing from CSS
    by devAlex

---
+++ code.js
@@ -0,0 +1,8 @@
+const gridContainer = document.querySelector(".grid-container");
+
+function gridDefault() {
+    let containerColumn = document.createElement("div");
+    for (let i = 0; i < 16; i++) {
+        
+    }
+}
