784f454d7b441a5f12d841dfa21445e365fca3de
dev-AlexSv <janalexandersilvestree@gmail.com>
2025-10-21 08:53:21 +0800

    Implemented script to render default grid boxes
    by devAlex

--- code.js
+++ code.js
@@ -1,8 +1,22 @@
-const gridContainer = document.querySelector(".grid-container");
+const body = document.querySelector("body");
+const container = document.querySelector(".grid-container");
+
+container.style["width"] = "480px";
+container.style["height"] = "480px";
+gridDefault();
 
 function gridDefault() {
-    let containerColumn = document.createElement("div");
-    for (let i = 0; i < 16; i++) {
-        
+    
+
+    for (let i = 0; i < 256; i++) {
+
+        let gridBox = document.createElement("div");
+        gridBox.className = "grid-box";
+
+        gridBox.style["width"] = "30px";
+        gridBox.style["height"] = "30px";
+
+        container.appendChild(gridBox);
     }
 }
+
