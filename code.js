7f71e5f661d7523cc54f6d1c98d4f3b845240d9a
dev-AlexSv <janalexandersilvestree@gmail.com>
2025-10-22 20:18:47 +0800

    Finalization Part 1
    By devAlex

--- code.js
+++ code.js
@@ -1,14 +1,5 @@
-const body = document.querySelector("body");
-const container = document.querySelector(".grid-container");
-const containerSize = 580;
-
-gridDefault();
-
-
-
-
 function gridDefault() {
-    
+    container.replaceChildren();
 
     for (let i = 0; i < 256; i++) {
 
@@ -20,6 +11,11 @@ function gridDefault() {
 
 
         container.appendChild(gridBox);
+    }
+    if (!blackWhiteMode) {
+        hoverGridBoxWithColor();
+    } else {
+        hoverGridBoxBlackAndWhiteOnly();
     }
 }
 
@@ -39,6 +35,79 @@ function gridWithChoice() {
         container.appendChild(gridBox);
     } 
 
-
+    if (!blackWhiteMode) {
+        hoverGridBoxWithColor();
+    } else {
+        hoverGridBoxBlackAndWhiteOnly();
+    }
 }
+
+function hoverGridBoxWithColor() {
+    const elements = document.querySelectorAll(".grid-box");
+
+    elements.forEach(gridBox => {
+        gridBox.addEventListener("mouseover", () => {
+            gridBox.style["background-color"] = allColorsMode();
+        })
+    })
+}
+
+function hoverGridBoxBlackAndWhiteOnly() {
+    const elements = document.querySelectorAll(".grid-box");
+
+    elements.forEach(gridBox => {
+        gridBox.addEventListener("mouseover", () => {
+            gridBox.style["background-color"] = blackAndWhiteMode(gridBox);
+            console.log(gridBox.style["background-color"]);
+        })
+    })
+}
+
+function allColorsMode() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+
+function blackAndWhiteMode(e) {
+    const element = e;
+    if (element.style["background-color"] === "rgb(255, 255, 255)") {
+        element.style["background-color"] = "rgb(0, 0, 0)";
+        
+    } else {
+        element.style["background-color"] = "#ffffff";
+    }
+}
+
+// MAIN CODE
+const body = document.querySelector("body");
+const container = document.querySelector(".grid-container");
+const containerSize = 580;
+
+const btSelectSize = document.querySelector("#bt-grid-select");
+const btReset = document.querySelector("#bt-reset");
+const btBWMode = document.querySelector("#bt-bw");
+
+let blackWhiteMode = false;
+
+gridDefault();
+
+
+
+btSelectSize.addEventListener('click', () => {
+    blackWhiteMode = false;
+    gridWithChoice();
+});
+btReset.addEventListener("click", () => {
+    blackWhiteMode = false;
+    gridDefault();
+});
+
+btBWMode.addEventListener("click", () => {
+    blackWhiteMode = true;
+    gridWithChoice();
+})
+
+
 
